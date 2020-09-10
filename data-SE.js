c6b2f54f50704b586cccf43c5b55d41be4e486e7
trustable-code <krauter.simon@arcor.de>
2020-09-11 01:01:32 +0200

    SE: Improve comment

--- data-SE.js
+++ data-SE.js
@@ -1181,7 +1181,7 @@ db.insert(db.countries.SE, "2020-06", db.dsTypes.ElectricCarsByModel, "http://ev
 , "Hyundai|Kona Electric": 100
 });
 
-// No source found for  2020-07
+// No source found for electric car sales 2020-07
 
 db.insert(db.countries.SE, "2020-08", db.dsTypes.AllCarsByBrand, "https://www.scb.se/en/finding-statistics/statistics-by-subject-area/transport-and-communications/road-traffic/registered-vehicles/pong/tables-and-graphs/registered-vehicles/ TODO: numbers per brand wanted",
 { "other": 26545
