f5a20669196a2e7b21c318542cb8cdde3612053e
trustable-code <krauter.simon@arcor.de>
2021-04-27 22:03:12 +0200

    Add data for Finland

---
+++ data-FI.js
@@ -0,0 +1,427 @@
+// Data sets for car sales in Finland
+
+"use strict";
+
+db.addCountry("FI", "Finland");
+
+db.insert(db.countries.FI, "2020-01", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20200218_PRPC_2001_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 10798
+});
+
+db.insert(db.countries.FI, "2020-01", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2020/1",
+{ "Renault|Zoé": 29
+, "Hyundai|Kona Electric": 25
+, "Nissan|Leaf": 21
+, "Audi|e-tron": 20
+, "Volkswagen|e-Golf": 16
+, "Volkswagen|e-up!": 13
+, "Tesla|Model 3": 13
+, "Tesla|Model S": 11
+, "Hyundai|Ioniq Electric": 11
+, "Seat|Mii Electric": 5
+, "Mercedes-Benz|EQC": 4
+, "Tesla|Model X": 3
+, "Nissan|e-NV200": 3
+, "Jaguar|I-Pace": 2
+, "BMW|i3 BEV": 2
+, "Kia|Soul EV": 1
+});
+
+db.insert(db.countries.FI, "2020-02", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20200318_PRPC_2002_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 8271
+});
+
+db.insert(db.countries.FI, "2020-02", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2020/2",
+{ "Seat|Mii Electric": 50
+, "Volkswagen|e-Golf": 35
+, "Volkswagen|e-up!": 32
+, "Hyundai|Kona Electric": 28
+, "Škoda|Citigo-e": 23
+, "Nissan|Leaf": 13
+, "Hyundai|Ioniq Electric": 8
+, "Audi|e-tron": 7
+, "Mercedes-Benz|EQC": 6
+, "Jaguar|I-Pace": 5
+, "Tesla|Model 3": 4
+, "Renault|Zoé": 4
+, "Mini|Cooper SE Electric": 4
+, "Kia|Soul EV": 3
+, "Tesla|Model S": 2
+, "Porsche|Taycan": 1
+, "BMW|i3 BEV": 1
+});
+
+db.insert(db.countries.FI, "2020-03", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20200417_PRPC_2003_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 9194
+});
+
+db.insert(db.countries.FI, "2020-03", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2020/3",
+{ "Tesla|Model 3": 273
+, "Seat|Mii Electric": 56
+, "Volkswagen|e-Golf": 30
+, "Hyundai|Kona Electric": 29
+, "Volkswagen|e-up!": 22
+, "Tesla|Model X": 15
+, "Tesla|Model S": 15
+, "Mercedes-Benz|EQC": 13
+, "Nissan|Leaf": 12
+, "Škoda|Citigo-e": 11
+, "Audi|e-tron": 10
+, "Jaguar|I-Pace": 7
+, "BMW|i3 BEV": 6
+, "Hyundai|Ioniq Electric": 4
+, "Porsche|Taycan": 3
+, "Mini|Cooper SE Electric": 2
+, "Kia|Soul EV": 2
+, "Renault|Zoé": 1
+});
+
+db.insert(db.countries.FI, "2020-04", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20200519_PRPC_2004-FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 5981
+});
+
+db.insert(db.countries.FI, "2020-04", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2020/4",
+{ "Tesla|Model 3": 34
+, "Kia|Niro EV": 21
+, "Volkswagen|e-Golf": 19
+, "Seat|Mii Electric": 17
+, "Hyundai|Kona Electric": 17
+, "Porsche|Taycan": 15
+, "Mercedes-Benz|EQC": 15
+, "Volkswagen|e-up!": 11
+, "Škoda|Citigo-e": 9
+, "Hyundai|Ioniq Electric": 9
+, "Tesla|Model S": 8
+, "Nissan|Leaf": 8
+, "Mini|Cooper SE Electric": 3
+, "BMW|i3 BEV": 3
+, "Kia|Soul EV": 2
+, "Tesla|Model X": 1
+, "Renault|Zoé": 1
+, "Jaguar|I-Pace": 1
+, "Audi|e-tron": 1
+});
+
+db.insert(db.countries.FI, "2020-05", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20200617_PRPC_2005_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 5110
+});
+
+db.insert(db.countries.FI, "2020-05", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2020/5",
+{ "Tesla|Model 3": 17
+, "Hyundai|Ioniq Electric": 16
+, "Seat|Mii Electric": 11
+, "Kia|Niro EV": 11
+, "Hyundai|Kona Electric": 8
+, "Porsche|Taycan": 6
+, "Nissan|Leaf": 6
+, "Volkswagen|e-up!": 5
+, "Tesla|Model X": 5
+, "Škoda|Citigo-e": 4
+, "Volkswagen|e-Golf": 4
+, "Tesla|Model S": 4
+, "Mercedes-Benz|EQC": 4
+, "Renault|Zoé": 3
+, "Audi|e-tron": 3
+, "BMW|i3 BEV": 1
+});
+
+db.insert(db.countries.FI, "2020-06", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20200616_PRPC_2006_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 8023
+});
+
+db.insert(db.countries.FI, "2020-06", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2020/6",
+{ "Volkswagen|e-up!": 59
+, "Tesla|Model 3": 47
+, "Volkswagen|e-Golf": 35
+, "Seat|Mii Electric": 30
+, "Hyundai|Ioniq Electric": 18
+, "Škoda|Citigo-e": 14
+, "Hyundai|Kona Electric": 14
+, "Renault|Zoé": 11
+, "Porsche|Taycan": 11
+, "Tesla|Model X": 5
+, "Tesla|Model S": 5
+, "Mercedes-Benz|EQC": 5
+, "BMW|i3 BEV": 5
+, "Audi|e-tron": 4
+, "Nissan|Leaf": 3
+, "Kia|Niro EV": 3
+, "Opel|Corsa-e": 2
+, "Mini|Cooper SE Electric": 1
+, "Jaguar|I-Pace": 1
+});
+
+db.insert(db.countries.FI, "2020-07", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20200917_PRPC_2007-08_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 9104
+});
+
+db.insert(db.countries.FI, "2020-07", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2020/7",
+{ "Volkswagen|e-up!": 50
+, "Hyundai|Kona Electric": 32
+, "Seat|Mii Electric": 29
+, "Hyundai|Ioniq Electric": 28
+, "Volkswagen|e-Golf": 15
+, "Audi|e-tron": 15
+, "Škoda|Citigo-e": 9
+, "Porsche|Taycan": 9
+, "Renault|Zoé": 7
+, "Mercedes-Benz|EQC": 7
+, "Nissan|Leaf": 5
+, "Mini|Cooper SE Electric": 5
+, "Opel|Corsa-e": 3
+, "Kia|Niro EV": 3
+, "BMW|i3 BEV": 3
+, "Peugeot|e-208": 2
+, "Jaguar|I-Pace": 2
+, "Tesla|Model X": 1
+, "Honda|e": 1
+});
+
+db.insert(db.countries.FI, "2020-08", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20200917_PRPC_2007-08_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 8485
+});
+
+db.insert(db.countries.FI, "2020-08", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2020/8",
+{ "Tesla|Model 3": 32
+, "Porsche|Taycan": 32
+, "Volkswagen|e-up!": 30
+, "Kia|Niro EV": 25
+, "Seat|Mii Electric": 22
+, "Škoda|Citigo-e": 20
+, "Nissan|Leaf": 19
+, "Hyundai|Kona Electric": 18
+, "Audi|e-tron": 18
+, "Mini|Cooper SE Electric": 12
+, "Mercedes-Benz|EQC": 11
+, "Hyundai|Ioniq Electric": 9
+, "Peugeot|e-208": 6
+, "Renault|Zoé": 5
+, "Jaguar|I-Pace": 5
+, "Volkswagen|e-Golf": 3
+, "Kia|Soul EV": 3
+, "BMW|i3 BEV": 3
+, "Peugeot|e-2008": 2
+, "Opel|Corsa-e": 2
+, "Honda|e": 1
+});
+
+db.insert(db.countries.FI, "2020-09", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20201016_PRPC_2009_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 8424
+});
+
+db.insert(db.countries.FI, "2020-09", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2020/9",
+{ "Volkswagen|ID.3": 201
+, "Tesla|Model 3": 182
+, "Hyundai|Ioniq Electric": 35
+, "Tesla|Model X": 31
+, "Tesla|Model S": 31
+, "Kia|Niro EV": 27
+, "Hyundai|Kona Electric": 27
+, "Volkswagen|e-up!": 19
+, "Škoda|Citigo-e": 18
+, "Seat|Mii Electric": 16
+, "Nissan|Leaf": 15
+, "Opel|Corsa-e": 12
+, "Porsche|Taycan": 11
+, "Renault|Zoé": 10
+, "Audi|e-tron": 9
+, "Peugeot|e-2008": 8
+, "Peugeot|e-208": 6
+, "Mazda|MX-30": 5
+, "Mercedes-Benz|EQC": 4
+, "Honda|e": 4
+, "Mini|Cooper SE Electric": 2
+, "Jaguar|I-Pace": 2
+, "BMW|i3 BEV": 2
+, "Nissan|e-NV200": 1
+});
+
+db.insert(db.countries.FI, "2020-10", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20201118_PRPC_2010_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 7499
+});
+
+db.insert(db.countries.FI, "2020-10", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2020/10",
+{ "Volkswagen|ID.3": 50
+, "Kia|Niro EV": 23
+, "Škoda|Citigo-e": 19
+, "Volkswagen|e-up!": 14
+, "Audi|e-tron": 14
+, "Peugeot|e-2008": 13
+, "Mini|Cooper SE Electric": 13
+, "Seat|Mii Electric": 11
+, "Hyundai|Kona Electric": 11
+, "Nissan|Leaf": 10
+, "Tesla|Model S": 9
+, "Tesla|Model 3": 8
+, "Opel|Corsa-e": 8
+, "Mercedes-Benz|EQC": 8
+, "Peugeot|e-208": 7
+, "Renault|Zoé": 6
+, "Tesla|Model X": 4
+, "Porsche|Taycan": 4
+, "Kia|Soul EV": 4
+, "Hyundai|Ioniq Electric": 3
+, "BMW|i3 BEV": 2
+, "Mazda|MX-30": 1
+});
+
+db.insert(db.countries.FI, "2020-11", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20201217_PRPC_2011_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 7374
+});
+
+db.insert(db.countries.FI, "2020-11", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2020/11",
+{ "Volkswagen|ID.3": 53
+, "Volkswagen|e-up!": 40
+, "Kia|Niro EV": 27
+, "Seat|Mii Electric": 18
+, "Hyundai|Kona Electric": 18
+, "Nissan|Leaf": 16
+, "Mini|Cooper SE Electric": 14
+, "Peugeot|e-2008": 13
+, "Škoda|Citigo-e": 11
+, "Audi|e-tron": 10
+, "Tesla|Model 3": 8
+, "Peugeot|e-208": 8
+, "Mercedes-Benz|EQC": 7
+, "Hyundai|Ioniq Electric": 5
+, "Tesla|Model S": 4
+, "Renault|Zoé": 3
+, "Porsche|Taycan": 3
+, "Opel|Corsa-e": 3
+, "Volvo|XC40 Recharge pure electric": 1
+, "Nissan|e-NV200": 1
+, "Mazda|MX-30": 1
+, "Kia|Soul EV": 1
+});
+
+db.insert(db.countries.FI, "2020-12", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/2021119_PRPC_2012_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 8132
+});
+
+db.insert(db.countries.FI, "2020-12", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2020/12",
+{ "Volkswagen|ID.3": 194
+, "Tesla|Model 3": 169
+, "Volvo|XC40 Recharge pure electric": 153
+, "Hyundai|Kona Electric": 107
+, "Nissan|Leaf": 82
+, "Renault|Zoé": 62
+, "Volkswagen|e-up!": 34
+, "Jaguar|I-Pace": 34
+, "Tesla|Model S": 30
+, "Kia|Niro EV": 26
+, "Tesla|Model X": 21
+, "Mazda|MX-30": 19
+, "Mercedes-Benz|EQV": 16
+, "Opel|Corsa-e": 14
+, "Škoda|Citigo-e": 12
+, "Seat|Mii Electric": 11
+, "Honda|e": 11
+, "Hyundai|Ioniq Electric": 10
+, "Mercedes-Benz|EQC": 9
+, "Porsche|Taycan": 7
+, "Mini|Cooper SE Electric": 6
+, "Audi|e-tron": 6
+, "Peugeot|e-2008": 5
+, "Mercedes-Benz|eVito": 5
+, "Peugeot|e-208": 2
+, "Ford|Mustang Mach-E": 2
+, "BMW|i3 BEV": 2
+, "Volkswagen|e-Golf": 1
+, "Nissan|e-NV200": 1
+});
+
+db.insert(db.countries.FI, "2021-01", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20210217_PRPC_2101_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 9373
+});
+
+db.insert(db.countries.FI, "2021-01", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2021/1",
+{ "Volvo|XC40 Recharge pure electric": 105
+, "Hyundai|Kona Electric": 48
+, "Audi|e-tron": 29
+, "Mercedes-Benz|EQC": 25
+, "Kia|Niro EV": 24
+, "Opel|Corsa-e": 16
+, "Volkswagen|ID.3": 14
+, "Škoda|Citigo-e": 13
+, "Nissan|Leaf": 13
+, "Seat|Mii Electric": 12
+, "Tesla|Model 3": 7
+, "Renault|Zoé": 6
+, "Peugeot|e-208": 6
+, "Peugeot|e-2008": 6
+, "Mini|Cooper SE Electric": 5
+, "Volkswagen|e-up!": 4
+, "Porsche|Taycan": 4
+, "Citroën|ë-C4": 4
+, "Nissan|e-NV200": 3
+, "Mazda|MX-30": 3
+, "Hyundai|Ioniq Electric": 3
+, "Kia|Soul EV": 2
+, "Mercedes-Benz|EQV": 1
+, "BMW|iX3": 1
+});
+
+db.insert(db.countries.FI, "2021-02", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20210317_PRPC_2102_FINAL_.pdf TODO: numbers per brand wanted",
+{ "other": 8170
+});
+
+db.insert(db.countries.FI, "2021-02", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2021/2",
+{ "Tesla|Model 3": 58
+, "Seat|Mii Electric": 43
+, "Volkswagen|ID.3": 39
+, "Volvo|XC40 Recharge pure electric": 37
+, "Volkswagen|ID.4": 32
+, "Hyundai|Kona Electric": 22
+, "Nissan|Leaf": 21
+, "Audi|e-tron": 20
+, "Kia|Niro EV": 19
+, "Peugeot|e-2008": 13
+, "Porsche|Taycan": 9
+, "Mercedes-Benz|EQC": 9
+, "Hyundai|Ioniq Electric": 6
+, "BMW|i3 BEV": 5
+, "Volkswagen|e-up!": 4
+, "Peugeot|e-208": 4
+, "Škoda|Citigo-e": 3
+, "Opel|Corsa-e": 3
+, "Mazda|MX-30": 3
+, "Mini|Cooper SE Electric": 2
+, "Jaguar|I-Pace": 2
+, "Ford|Mustang Mach-E": 2
+, "Citroën|ë-C4": 2
+, "BMW|iX3": 1
+});
+
+db.insert(db.countries.FI, "2021-03", db.dsTypes.AllCarsByBrand, "https://www.acea.be/uploads/press_releases_files/20210416_PRPC_2103_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 10428
+});
+
+db.insert(db.countries.FI, "2021-03", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/FI/Brands/Month/2021/3",
+{ "Tesla|Model 3": 298
+, "Volkswagen|ID.4": 94
+, "Volkswagen|ID.3": 64
+, "Seat|Mii Electric": 60
+, "Nissan|Leaf": 57
+, "Volvo|XC40 Recharge pure electric": 36
+, "Kia|Niro EV": 28
+, "Volkswagen|e-up!": 15
+, "Audi|e-tron": 15
+, "Mercedes-Benz|EQC": 13
+, "Porsche|Taycan": 12
+, "Peugeot|e-2008": 12
+, "Hyundai|Kona Electric": 9
+, "BMW|iX3": 9
+, "Peugeot|e-208": 8
+, "Lexus|UX 300e": 7
+, "Hyundai|Ioniq Electric": 7
+, "BMW|i3 BEV": 7
+, "Škoda|Citigo-e": 5
+, "Polestar|2": 5
+, "Opel|Corsa-e": 3
+, "Mazda|MX-30": 3
+, "Citroën|ë-C4": 3
+, "Škoda|Enyaq": 1
+, "Mini|Cooper SE Electric": 1
+, "Mercedes-Benz|EQA": 1
+});
+
