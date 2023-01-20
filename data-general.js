b29a4c7785c25780c6b5d24dac2c99875cd4667e
simonkrauter <trustable@disroot.org>
2023-01-20 11:07:19 -0300

    Move country-independent data to own file

---
+++ data-general.js
@@ -0,0 +1,42 @@
+// Country-independent data
+
+"use strict";
+
+var colorSet =
+[ "#E83835"
+, "#1469AA"
+, "#43A047"
+, "#F78300"
+, "#31CEE0"
+, "#FFCC00"
+, "#8025A8"
+, "#8C8C8C"
+, "#16545B"
+, "#795548"
+, "#A00000"
+, "#000E8E"
+, "#024C00"
+, "#8C4D00"
+, "#87711C"
+, "#93F296"
+, "#8B66DD"
+, "#C0CA33"
+, "#E52984"
+, "#FFBB68"
+];
+
+var colorIndexByBrand =
+{ "Tesla": 0
+, "Nissan": 7
+, "Chevrolet": 5
+, "Renault": 3
+, "Volkswagen": 1
+, "Smart": 2
+, "BMW": 4
+, "Kia": 10
+, "Hyundai": 11
+, "Fiat": 18
+, "Jaguar": 8
+, "Audi": 6
+, "Peugeot": 9
+};
--- frontend.js
+++ frontend.js
@@ -627,44 +627,6 @@ function addPngExportButton(parent) {
 }
 
 function getChartSeriesColors(chartConfig, chartData) {
-  var colorSet = [
-    "#E83835",
-    "#1469AA",
-    "#43A047",
-    "#F78300",
-    "#31CEE0",
-    "#FFCC00",
-    "#8025A8",
-    "#8C8C8C",
-    "#16545B",
-    "#795548",
-    "#A00000",
-    "#000E8E",
-    "#024C00",
-    "#8C4D00",
-    "#87711C",
-    "#93F296",
-    "#8B66DD",
-    "#C0CA33",
-    "#E52984",
-    "#FFBB68"
-  ];
-  var colorIndexByBrand = {
-    "Tesla": 0,
-    "Nissan": 7,
-    "Chevrolet": 5,
-    "Renault": 3,
-    "Volkswagen": 1,
-    "Smart": 2,
-    "BMW": 4,
-    "Kia": 10,
-    "Hyundai": 11,
-    "Fiat": 18,
-    "Jaguar": 8,
-    "Audi": 6,
-    "Peugeot": 9
-  };
-
   var colors = [];
   var usedIndexes = [];
   if (![db.xProperties.brand, db.xProperties.model, db.xProperties.country].includes(chartConfig.xProperty) && chartConfig.brand == db.brandOptions.all && chartConfig.model == db.modelOptions.combine) {
