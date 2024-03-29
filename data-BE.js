c243e9e2589a7225f6bd2fa338527af26b6eb61f
simonkrauter <trustable@disroot.org>
2024-03-29 11:55:44 -0300

    Add data for Belgium

---
+++ data-BE.js
@@ -0,0 +1,483 @@
+// Data sets for car sales in Belgium
+
+"use strict";
+
+db.insert(db.countries.BE, "2023-01", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/20230221_PRPC_2301_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 39847
+});
+
+db.insert(db.countries.BE, "2023-01", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/BE/Brands/Month/2023/1",
+{ "Tesla|Model Y": 430
+, "Volkswagen|ID.3": 353
+, "Audi|Q4 e-tron": 341
+, "Porsche|Taycan": 336
+, "Audi|Q8 e-tron": 224
+, "Volkswagen|ID.4": 220
+, "Mercedes-Benz|EQA": 184
+, "Mercedes-Benz|EQC": 172
+, "Polestar|2": 138
+, "Hyundai|Ioniq 5": 135
+, "BMW|iX": 134
+, "Ford|Mustang Mach-E": 122
+, "Tesla|Model 3": 107
+, "BMW|iX3": 105
+, "Škoda|Enyaq": 104
+, "Mercedes-Benz|EQB": 77
+, "Dacia|Spring": 67
+, "Kia|EV6": 66
+, "Tesla|Model S": 55
+, "Mercedes-Benz|EQS": 43
+, "Nissan|Ariya": 28
+, "Nissan|Leaf": 20
+, "Tesla|Model X": 17
+, "MG|MG5 EV": 16
+, "Renault|Zoe": 14
+, "Volkswagen|e-up!": 14
+, "Toyota|bZ4X": 13
+, "Smart|Fortwo ED": 9
+, "Mercedes-Benz|EQV": 7
+, "Jaguar|I-Pace": 5
+, "Mazda|MX-30 BEV": 4
+, "other": 2136
+});
+
+db.insert(db.countries.BE, "2023-02", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/20230321_PRPC_2302-FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 39883
+});
+
+db.insert(db.countries.BE, "2023-02", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/BE/Brands/Month/2023/2",
+{ "Tesla|Model Y": 663
+, "Audi|Q4 e-tron": 459
+, "Volkswagen|ID.3": 343
+, "Volkswagen|ID.4": 337
+, "Mercedes-Benz|EQA": 177
+, "Porsche|Taycan": 156
+, "Tesla|Model 3": 136
+, "Mercedes-Benz|EQB": 124
+, "Škoda|Enyaq": 117
+, "Mercedes-Benz|EQC": 101
+, "Polestar|2": 88
+, "BMW|iX": 87
+, "Hyundai|Ioniq 5": 86
+, "Audi|Q8 e-tron": 68
+, "Ford|Mustang Mach-E": 65
+, "Kia|EV6": 63
+, "Mercedes-Benz|EQS": 51
+, "Dacia|Spring": 44
+, "Nissan|Ariya": 29
+, "Nissan|Leaf": 26
+, "BMW|iX3": 17
+, "Renault|Zoe": 17
+, "Toyota|bZ4X": 17
+, "Volkswagen|e-up!": 13
+, "Mercedes-Benz|EQV": 10
+, "Smart|Fortwo ED": 9
+, "MG|MG5 EV": 8
+, "Tesla|Model S": 8
+, "Jaguar|I-Pace": 7
+, "Tesla|Model X": 5
+, "Hyundai|Ioniq Electric": 3
+, "Mazda|MX-30 BEV": 3
+, "other": 2287
+});
+
+db.insert(db.countries.BE, "2023-03", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/20230419_PRPC_2303-FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 51754
+});
+
+db.insert(db.countries.BE, "2023-03", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/BE/Brands/Month/2023/3",
+{ "Tesla|Model Y": 2047
+, "Tesla|Model 3": 908
+, "Volkswagen|ID.3": 521
+, "Audi|Q4 e-tron": 500
+, "Volkswagen|ID.4": 292
+, "Mercedes-Benz|EQA": 254
+, "Škoda|Enyaq": 181
+, "BMW|iX": 171
+, "Ford|Mustang Mach-E": 164
+, "MG|MG5 EV": 154
+, "Mercedes-Benz|EQB": 140
+, "Kia|EV6": 136
+, "Mercedes-Benz|EQC": 135
+, "Polestar|2": 115
+, "Porsche|Taycan": 93
+, "Hyundai|Ioniq 5": 83
+, "Audi|e-tron GT": 77
+, "BMW|iX3": 66
+, "Nissan|Leaf": 57
+, "Tesla|Model S": 50
+, "Dacia|Spring": 43
+, "Volkswagen|e-up!": 42
+, "Tesla|Model X": 40
+, "Renault|Zoe": 32
+, "Toyota|bZ4X": 25
+, "Mercedes-Benz|EQV": 24
+, "Nissan|Ariya": 21
+, "Audi|Q8 e-tron": 11
+, "Jaguar|I-Pace": 8
+, "Mercedes-Benz|EQS": 8
+, "other": 3061
+});
+
+db.insert(db.countries.BE, "2023-04", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/20230517_PRPC_2304_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 39872
+});
+
+db.insert(db.countries.BE, "2023-04", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/BE/Brands/Month/2023/4",
+{ "Tesla|Model Y": 585
+, "Volkswagen|ID.4": 434
+, "Audi|Q4 e-tron": 406
+, "Volkswagen|ID.3": 302
+, "Škoda|Enyaq": 283
+, "Polestar|2": 224
+, "Mercedes-Benz|EQA": 204
+, "Tesla|Model 3": 170
+, "Mercedes-Benz|EQB": 154
+, "Kia|EV6": 134
+, "Porsche|Taycan": 88
+, "Mercedes-Benz|EQC": 85
+, "BMW|iX": 70
+, "Tesla|Model X": 67
+, "Hyundai|Ioniq 5": 63
+, "BMW|iX3": 55
+, "Dacia|Spring": 51
+, "Ford|Mustang Mach-E": 48
+, "Tesla|Model S": 46
+, "Renault|Zoe": 32
+, "Volkswagen|e-up!": 23
+, "Mercedes-Benz|EQS": 22
+, "Toyota|bZ4X": 22
+, "Mazda|MX-30 BEV": 21
+, "MG|MG5 EV": 20
+, "Audi|Q8 e-tron": 18
+, "Nissan|Ariya": 15
+, "Nissan|Leaf": 14
+, "Jaguar|I-Pace": 9
+, "Mercedes-Benz|EQV": 9
+, "Smart|Fortwo ED": 8
+, "Audi|e-tron GT": 7
+, "other": 2797
+});
+
+db.insert(db.countries.BE, "2023-05", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/20230621_PRPC_2305_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 41359
+});
+
+db.insert(db.countries.BE, "2023-05", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/BE/Brands/Month/2023/5",
+{ "Tesla|Model Y": 786
+, "Volkswagen|ID.4": 574
+, "Audi|Q4 e-tron": 364
+, "Polestar|2": 304
+, "Škoda|Enyaq": 242
+, "Volkswagen|ID.3": 223
+, "Mercedes-Benz|EQA": 205
+, "Mercedes-Benz|EQB": 160
+, "Kia|EV6": 138
+, "Tesla|Model 3": 123
+, "Porsche|Taycan": 120
+, "Audi|Q8 e-tron": 119
+, "BMW|iX": 78
+, "Mercedes-Benz|EQC": 76
+, "Tesla|Model X": 57
+, "BMW|iX3": 55
+, "Ford|Mustang Mach-E": 44
+, "Hyundai|Ioniq 5": 37
+, "MG|MG5 EV": 35
+, "Renault|Zoe": 33
+, "Tesla|Model S": 29
+, "Toyota|bZ4X": 28
+, "Dacia|Spring": 26
+, "Nissan|Ariya": 21
+, "Audi|e-tron GT": 16
+, "Volkswagen|e-up!": 16
+, "Nissan|Leaf": 15
+, "Mazda|MX-30 BEV": 11
+, "Mercedes-Benz|EQV": 9
+, "Mercedes-Benz|EQS": 7
+, "Jaguar|I-Pace": 6
+, "Smart|Fortwo ED": 4
+, "other": 3233
+});
+
+db.insert(db.countries.BE, "2023-06", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/20230719_PRPC_2306-FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 51760
+});
+
+db.insert(db.countries.BE, "2023-06", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/BE/Brands/Month/2023/6",
+{ "Tesla|Model Y": 1296
+, "Volkswagen|ID.4": 618
+, "Tesla|Model 3": 463
+, "Audi|Q4 e-tron": 371
+, "Škoda|Enyaq": 333
+, "Mercedes-Benz|EQA": 245
+, "Polestar|2": 203
+, "Volkswagen|ID.3": 196
+, "Porsche|Taycan": 166
+, "Ford|Mustang Mach-E": 138
+, "Kia|EV6": 137
+, "BMW|iX": 125
+, "MG|MG5 EV": 80
+, "BMW|iX3": 71
+, "Tesla|Model X": 69
+, "Mercedes-Benz|EQB": 63
+, "Mercedes-Benz|EQC": 63
+, "Hyundai|Ioniq 5": 56
+, "Dacia|Spring": 51
+, "Tesla|Model S": 40
+, "Audi|e-tron GT": 35
+, "Nissan|Ariya": 33
+, "Nissan|Leaf": 28
+, "Volkswagen|e-up!": 28
+, "Toyota|bZ4X": 19
+, "Audi|Q8 e-tron": 16
+, "Mercedes-Benz|EQS": 15
+, "Renault|Zoe": 12
+, "Mercedes-Benz|EQV": 9
+, "Jaguar|I-Pace": 6
+, "other": 4134
+});
+
+db.insert(db.countries.BE, "2023-07", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/20230830_PRPC_2307_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 34669
+});
+
+db.insert(db.countries.BE, "2023-07", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/BE/Brands/Month/2023/7",
+{ "Tesla|Model Y": 963
+, "Volkswagen|ID.4": 506
+, "Škoda|Enyaq": 381
+, "Mercedes-Benz|EQA": 287
+, "Tesla|Model 3": 250
+, "Audi|Q4 e-tron": 206
+, "Volkswagen|ID.3": 205
+, "Polestar|2": 177
+, "BMW|iX3": 173
+, "Porsche|Taycan": 135
+, "Kia|EV6": 129
+, "BMW|iX": 82
+, "Ford|Mustang Mach-E": 73
+, "Mercedes-Benz|EQB": 65
+, "Mercedes-Benz|EQC": 65
+, "Hyundai|Ioniq 5": 55
+, "Dacia|Spring": 50
+, "MG|MG5 EV": 24
+, "Toyota|bZ4X": 24
+, "Volkswagen|e-up!": 21
+, "Nissan|Ariya": 19
+, "Audi|e-tron GT": 18
+, "Mercedes-Benz|EQS": 16
+, "Tesla|Model S": 14
+, "Nissan|Leaf": 11
+, "Audi|Q8 e-tron": 9
+, "Jaguar|I-Pace": 9
+, "Tesla|Model X": 8
+, "Renault|Zoe": 5
+, "other": 2751
+});
+
+db.insert(db.countries.BE, "2023-08", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/20230920_PRPC_2308_FINAL.pdf TODO: numbers per brand wanted",
+{ "other": 36798
+});
+
+db.insert(db.countries.BE, "2023-08", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/BE/Brands/Month/2023/8",
+{ "Tesla|Model Y": 1185
+, "Volkswagen|ID.3": 958
+, "Škoda|Enyaq": 612
+, "Volkswagen|ID.4": 553
+, "Audi|Q4 e-tron": 337
+, "Mercedes-Benz|EQA": 326
+, "BMW|iX3": 324
+, "Tesla|Model 3": 293
+, "Polestar|2": 227
+, "Kia|EV6": 163
+, "BMW|iX": 103
+, "Ford|Mustang Mach-E": 84
+, "Porsche|Taycan": 79
+, "Dacia|Spring": 64
+, "Nissan|Ariya": 61
+, "Mercedes-Benz|EQB": 60
+, "Mercedes-Benz|EQC": 60
+, "Hyundai|Ioniq 5": 37
+, "Toyota|bZ4X": 31
+, "Nissan|Leaf": 30
+, "Volkswagen|e-up!": 16
+, "Tesla|Model S": 15
+, "Audi|e-tron GT": 13
+, "MG|MG5 EV": 13
+, "Mercedes-Benz|EQV": 13
+, "Tesla|Model X": 12
+, "Renault|Zoe": 10
+, "Mercedes-Benz|EQS": 6
+, "Honda|e": 5
+, "other": 3551
+});
+
+db.insert(db.countries.BE, "2023-09", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/Press_release_car_registrations_September_2023.pdf TODO: numbers per brand wanted",
+{ "other": 38906
+});
+
+db.insert(db.countries.BE, "2023-09", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/BE/Brands/Month/2023/9",
+{ "Tesla|Model Y": 1025
+, "Audi|Q4 e-tron": 618
+, "Škoda|Enyaq": 612
+, "Mercedes-Benz|EQA": 538
+, "Volkswagen|ID.4": 427
+, "BMW|iX3": 326
+, "Kia|EV6": 246
+, "Volkswagen|ID.3": 212
+, "Polestar|2": 154
+, "Ford|Mustang Mach-E": 122
+, "Hyundai|Ioniq 5": 88
+, "Audi|Q8 e-tron": 86
+, "BMW|iX": 81
+, "Porsche|Taycan": 61
+, "Dacia|Spring": 56
+, "Nissan|Ariya": 51
+, "Toyota|bZ4X": 43
+, "Tesla|Model S": 42
+, "Mercedes-Benz|EQB": 40
+, "Mercedes-Benz|EQC": 40
+, "Tesla|Model X": 40
+, "MG|MG5 EV": 39
+, "Volkswagen|e-up!": 28
+, "Tesla|Model 3": 21
+, "Mercedes-Benz|EQS": 19
+, "Nissan|Leaf": 13
+, "Smart|Fortwo ED": 12
+, "Audi|e-tron GT": 9
+, "Mercedes-Benz|EQV": 8
+, "Renault|Zoe": 8
+, "Jaguar|I-Pace": 5
+, "other": 4076
+});
+
+db.insert(db.countries.BE, "2023-10", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/Press_release_car_registrations_October_2023.pdf TODO: numbers per brand wanted",
+{ "other": 38560
+});
+
+db.insert(db.countries.BE, "2023-10", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/BE/Brands/Month/2023/10",
+{ "Audi|Q4 e-tron": 778
+, "Tesla|Model Y": 671
+, "Škoda|Enyaq": 592
+, "Volkswagen|ID.4": 500
+, "Tesla|Model 3": 403
+, "Kia|EV6": 250
+, "Mercedes-Benz|EQA": 248
+, "Polestar|2": 220
+, "Volkswagen|ID.3": 191
+, "Mercedes-Benz|EQB": 187
+, "BMW|iX3": 185
+, "Ford|Mustang Mach-E": 100
+, "Hyundai|Ioniq 5": 95
+, "MG|MG5 EV": 82
+, "Porsche|Taycan": 76
+, "BMW|iX": 56
+, "Nissan|Ariya": 54
+, "Toyota|bZ4X": 50
+, "Dacia|Spring": 49
+, "Mercedes-Benz|EQC": 24
+, "Volkswagen|e-up!": 22
+, "Tesla|Model S": 20
+, "Renault|Zoe": 16
+, "Audi|Q8 e-tron": 15
+, "Smart|Fortwo ED": 12
+, "Mercedes-Benz|EQS": 10
+, "Nissan|Leaf": 10
+, "Tesla|Model X": 10
+, "Mercedes-Benz|EQV": 9
+, "Audi|e-tron GT": 6
+, "other": 3687
+});
+
+db.insert(db.countries.BE, "2023-11", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/Press_release_car_registrations_November_2023.pdf TODO: numbers per brand wanted",
+{ "other": 37552
+});
+
+db.insert(db.countries.BE, "2023-11", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/BE/Brands/Month/2023/11",
+{ "Audi|Q4 e-tron": 958
+, "Tesla|Model Y": 795
+, "Volkswagen|ID.4": 698
+, "Tesla|Model 3": 567
+, "Polestar|2": 510
+, "Škoda|Enyaq": 482
+, "Mercedes-Benz|EQA": 390
+, "Mercedes-Benz|EQB": 333
+, "Kia|EV6": 171
+, "Hyundai|Ioniq 5": 162
+, "Volkswagen|ID.3": 158
+, "BMW|iX": 105
+, "BMW|iX3": 105
+, "Ford|Mustang Mach-E": 97
+, "Volkswagen|e-up!": 55
+, "Tesla|Model X": 54
+, "Tesla|Model S": 44
+, "Porsche|Taycan": 37
+, "MG|MG5 EV": 34
+, "Dacia|Spring": 32
+, "Audi|Q8 e-tron": 24
+, "Mercedes-Benz|EQC": 23
+, "Nissan|Ariya": 22
+, "Mazda|MX-30 BEV": 20
+, "Toyota|bZ4X": 20
+, "Mercedes-Benz|EQV": 17
+, "Mercedes-Benz|EQS": 13
+, "Nissan|Leaf": 13
+, "Audi|e-tron GT": 9
+, "Smart|Fortwo ED": 8
+, "Jaguar|I-Pace": 6
+, "other": 3537
+});
+
+db.insert(db.countries.BE, "2023-12", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/Press_release_car_registrations_full_year_2023.pdf TODO: numbers per brand wanted",
+{ "other": 25715
+});
+
+db.insert(db.countries.BE, "2023-12", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/BE/Brands/Month/2023/12",
+{ "Tesla|Model Y": 1043
+, "Volkswagen|ID.4": 323
+, "Audi|Q4 e-tron": 285
+, "Tesla|Model 3": 250
+, "Polestar|2": 204
+, "Mercedes-Benz|EQB": 154
+, "Škoda|Enyaq": 153
+, "Mercedes-Benz|EQA": 152
+, "BMW|iX3": 126
+, "BMW|iX": 119
+, "Volkswagen|ID.3": 109
+, "Kia|EV6": 91
+, "MG|MG5 EV": 83
+, "Ford|Mustang Mach-E": 80
+, "Mazda|MX-30 BEV": 73
+, "Hyundai|Ioniq 5": 54
+, "Tesla|Model X": 48
+, "Tesla|Model S": 42
+, "Porsche|Taycan": 24
+, "Nissan|Ariya": 21
+, "Dacia|Spring": 18
+, "Volkswagen|e-up!": 16
+, "Audi|e-tron GT": 15
+, "Mercedes-Benz|EQC": 12
+, "Audi|Q8 e-tron": 10
+, "Mercedes-Benz|EQS": 10
+, "Renault|Zoe": 9
+, "Smart|Fortwo ED": 9
+, "Mercedes-Benz|EQV": 8
+, "Nissan|Leaf": 8
+, "Toyota|bZ4X": 8
+, "other": 2905
+});
+
+db.insert(db.countries.BE, "2024-01", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/Press_release_car_registrations-January_2024.pdf TODO: numbers per brand wanted",
+{ "other": 46667
+});
+
+db.insert(db.countries.BE, "2024-01", db.dsTypes.ElectricCarsByBrand, "https://www.acea.auto/files/Press_release_car_registrations-January_2024.pdf TODO: numbers per model wanted",
+{ "other": 9995
+});
+
+db.insert(db.countries.BE, "2024-02", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/Press_release_car_registrations_February_2024.pdf TODO: numbers per brand wanted",
+{ "other": 43723
+});
+
+db.insert(db.countries.BE, "2024-02", db.dsTypes.ElectricCarsByBrand, "https://www.acea.auto/files/Press_release_car_registrations_February_2024.pdf TODO: numbers per model wanted",
+{ "other": 9385
+});
