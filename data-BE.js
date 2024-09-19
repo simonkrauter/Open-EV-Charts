ed475ee6020460ea3f08443bdc762f2668325c13
simonkrauter <trustable@disroot.org>
2024-09-19 14:07:00 -0300

    BE: Add data for 2024-08

--- data-BE.js
+++ data-BE.js
@@ -521,3 +521,11 @@ db.insert(db.countries.BE, "2024-07", db.dsTypes.AllCarsByBrand, "https://www.ac
 db.insert(db.countries.BE, "2024-07", db.dsTypes.ElectricCarsByBrand, "https://www.acea.auto/files/Press_release_car_registrations_July_2024.pdf TODO: numbers per brand wanted",
 { "other": 9706
 });
+
+db.insert(db.countries.BE, "2024-08", db.dsTypes.AllCarsByBrand, "https://www.acea.auto/files/Press_release_car_registrations_August-2024.pdf TODO: numbers per brand wanted",
+{ "other": 29333
+});
+
+db.insert(db.countries.BE, "2024-08", db.dsTypes.ElectricCarsByBrand, "https://www.acea.auto/files/Press_release_car_registrations_August-2024.pdf TODO: numbers per brand wanted",
+{ "other": 10027
+});
