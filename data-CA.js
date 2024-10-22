7ff307d44d8c4aa4858bb78095251f22da7914bb
simonkrauter <trustable@disroot.org>
2024-10-21 21:21:49 -0300

    Add data for Canada

---
+++ data-CA.js
@@ -0,0 +1,243 @@
+// Data sets for car sales in Canada
+
+"use strict";
+
+db.insert(db.countries.CA, "2017-Q1", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 425043
+});
+
+db.insert(db.countries.CA, "2017-Q1", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 1664
+});
+
+db.insert(db.countries.CA, "2017-Q2", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 611018
+});
+
+db.insert(db.countries.CA, "2017-Q2", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 2195
+});
+
+db.insert(db.countries.CA, "2017-Q3", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 552329
+});
+
+db.insert(db.countries.CA, "2017-Q3", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 2400
+});
+
+db.insert(db.countries.CA, "2017-Q4", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 450846
+});
+
+db.insert(db.countries.CA, "2017-Q4", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 2820
+});
+
+db.insert(db.countries.CA, "2018-Q1", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 406120
+});
+
+db.insert(db.countries.CA, "2018-Q1", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 2639
+});
+
+db.insert(db.countries.CA, "2018-Q2", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 607876
+});
+
+db.insert(db.countries.CA, "2018-Q2", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 7393
+});
+
+db.insert(db.countries.CA, "2018-Q3", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 539754
+});
+
+db.insert(db.countries.CA, "2018-Q3", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 6506
+});
+
+db.insert(db.countries.CA, "2018-Q4", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 422110
+});
+
+db.insert(db.countries.CA, "2018-Q4", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 6032
+});
+
+db.insert(db.countries.CA, "2019-Q1", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 402123
+});
+
+db.insert(db.countries.CA, "2019-Q1", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 5273
+});
+
+db.insert(db.countries.CA, "2019-Q2", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 575763
+});
+
+db.insert(db.countries.CA, "2019-Q2", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 12370
+});
+
+db.insert(db.countries.CA, "2019-Q3", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 530607
+});
+
+db.insert(db.countries.CA, "2019-Q3", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 10120
+});
+
+db.insert(db.countries.CA, "2019-Q4", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 421952
+});
+
+db.insert(db.countries.CA, "2019-Q4", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 7760
+});
+
+db.insert(db.countries.CA, "2020-Q1", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 336214
+});
+
+db.insert(db.countries.CA, "2020-Q1", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 8386
+});
+
+db.insert(db.countries.CA, "2020-Q2", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 305911
+});
+
+db.insert(db.countries.CA, "2020-Q2", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 6088
+});
+
+db.insert(db.countries.CA, "2020-Q3", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 508642
+});
+
+db.insert(db.countries.CA, "2020-Q3", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 12601
+});
+
+db.insert(db.countries.CA, "2020-Q4", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 394794
+});
+
+db.insert(db.countries.CA, "2020-Q4", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 11961
+});
+
+db.insert(db.countries.CA, "2021-Q1", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 375826
+});
+
+db.insert(db.countries.CA, "2021-Q1", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 12693
+});
+
+db.insert(db.countries.CA, "2021-Q2", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 486592
+});
+
+db.insert(db.countries.CA, "2021-Q2", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 16167
+});
+
+db.insert(db.countries.CA, "2021-Q3", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 446620
+});
+
+db.insert(db.countries.CA, "2021-Q3", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 15845
+});
+
+db.insert(db.countries.CA, "2021-Q4", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 337566
+});
+
+db.insert(db.countries.CA, "2021-Q4", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 14247
+});
+
+db.insert(db.countries.CA, "2022-Q1", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 340149
+});
+
+db.insert(db.countries.CA, "2022-Q1", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 19705
+});
+
+db.insert(db.countries.CA, "2022-Q2", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 431985
+});
+
+db.insert(db.countries.CA, "2022-Q2", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 21764
+});
+
+db.insert(db.countries.CA, "2022-Q3", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 394492
+});
+
+db.insert(db.countries.CA, "2022-Q3", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 29376
+});
+
+db.insert(db.countries.CA, "2022-Q4", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 346478
+});
+
+db.insert(db.countries.CA, "2022-Q4", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 27775
+});
+
+db.insert(db.countries.CA, "2023-Q1", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 355885
+});
+
+db.insert(db.countries.CA, "2023-Q1", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 24216
+});
+
+db.insert(db.countries.CA, "2023-Q2", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 473577
+});
+
+db.insert(db.countries.CA, "2023-Q2", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 35631
+});
+
+db.insert(db.countries.CA, "2023-Q3", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 457343
+});
+
+db.insert(db.countries.CA, "2023-Q3", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 43763
+});
+
+db.insert(db.countries.CA, "2023-Q4", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 428108
+});
+
+db.insert(db.countries.CA, "2023-Q4", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 40051
+});
+
+db.insert(db.countries.CA, "2024-Q1", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 414876
+});
+
+db.insert(db.countries.CA, "2024-Q1", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 35771
+});
+
+db.insert(db.countries.CA, "2024-Q2", db.dsTypes.AllCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 511173
+});
+
+db.insert(db.countries.CA, "2024-Q2", db.dsTypes.ElectricCarsTotal, "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010002401",
+{ "other": 48489
+});
