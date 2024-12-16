1a5c78d03835cf4d7c8f5270457b87c11c190cf3
simonkrauter <trustable@disroot.org>
2024-12-16 19:38:45 +0100

    DK: Add data for 2024-10

--- data-DK.js
+++ data-DK.js
@@ -3028,3 +3028,77 @@ db.insert(db.countries.DK, "2024-09", db.dsTypes.ElectricCarsByModel, "https://e
 , "Subaru|other": 7
 , "other": 62
 });
+
+db.insert(db.countries.DK, "2024-10", db.dsTypes.AllCarsTotal, "https://www.acea.auto/files/Press_release_car_registrations_October_2024.pdf",
+{ "other": 14409
+});
+
+db.insert(db.countries.DK, "2024-10", db.dsTypes.ElectricCarsByModel, "https://eu-evs.com/bestSellers/DK/Brands/Month/2024/10, https://www.acea.auto/files/Press_release_car_registrations_October_2024.pdf",
+{ "Škoda|Enyaq": 1053
+, "Ford|Explorer EV": 627
+, "Volkswagen|ID.4": 601
+, "Tesla|Model Y": 525
+, "Audi|Q4 e-tron": 426
+, "Volkswagen|ID.3": 371
+, "Volkswagen|ID.7": 356
+, "Mercedes-Benz|EQA": 284
+, "Volvo|EX40": 262
+, "Volvo|EX30": 230
+, "Audi|Q6 e-tron": 212
+, "Tesla|Model 3": 205
+, "Volkswagen|ID. Buzz": 195
+, "BYD|Dolphin": 192
+, "Cupra|Born": 178
+, "Nissan|Ariya": 157
+, "Peugeot|E-3008": 152
+, "Toyota|bZ4X": 151
+, "Mercedes-Benz|EQB": 144
+, "BMW|i5": 134
+, "Mercedes-Benz|EQE": 126
+, "Renault|Scénic E-Tech": 119
+, "Hyundai|Kona Electric": 117
+, "Porsche|Macan Electric": 113
+, "Volkswagen|ID.5": 106
+, "Hyundai|Ioniq 5": 101
+, "BMW|iX1": 100
+, "Peugeot|E-5008": 94
+, "Kia|EV6": 89
+, "Polestar|4": 85
+, "MG|MG4 EV": 79
+, "Kia|Niro EV": 77
+, "BMW|iX3": 69
+, "Mini|Cooper SE": 68
+, "Renault|Megane E-Tech": 68
+, "BMW|i4": 67
+, "Polestar|2": 60
+, "Ford|Mustang Mach-E": 59
+, "Cupra|Tavascan": 57
+, "Volvo|EC40": 51
+, "BYD|Song Plus": 48
+, "Ford|E-Transit": 48
+, "Hyundai|Ioniq 6": 48
+, "Peugeot|e-2008": 42
+, "BMW|iX2": 41
+, "BYD|Seal": 40
+, "Mercedes-Benz|eVito": 39
+, "Fiat|500e": 35
+, "Peugeot|e-Expert": 32
+, "Mercedes-Benz|eCitan": 28
+, "Opel|other": 56
+, "Kia|other": 30
+, "Honda|other": 24
+, "BMW|other": 20
+, "Ford|other": 19
+, "Toyota|other": 17
+, "Peugeot|other": 16
+, "BYD|other": 15
+, "Citroën|other": 15
+, "MG|other": 14
+, "Mercedes-Benz|other": 14
+, "Renault|other": 13
+, "Mazda|other": 12
+, "Nissan|other": 12
+, "Polestar|other": 8
+, "Porsche|other": 7
+, "other": 530
+});
