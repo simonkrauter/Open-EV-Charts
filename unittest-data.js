552c7490b514722366c9502c346373d28c1e1b85
simonkrauter <trustable@disroot.org>
2025-03-27 10:50:52 -0300

    Add unit tests for backend

---
+++ unittest-data.js
@@ -0,0 +1,107 @@
+"use strict";
+
+db.currentDate = new Date("2025-01-15");
+
+db.insert(db.countries.BR, "2023-12", db.dsTypes.AllCarsTotal, "",
+{ "other": 1010
+});
+
+db.insert(db.countries.BR, "2023-12", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 10
+});
+
+db.insert(db.countries.BR, "2024-01", db.dsTypes.AllCarsTotal, "",
+{ "other": 1020
+});
+
+db.insert(db.countries.BR, "2024-01", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 2
+});
+
+db.insert(db.countries.BR, "2024-02", db.dsTypes.AllCarsTotal, "",
+{ "other": 1040
+});
+
+db.insert(db.countries.BR, "2024-02", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 4
+});
+
+db.insert(db.countries.BR, "2024-03", db.dsTypes.AllCarsTotal, "",
+{ "other": 1060
+});
+
+db.insert(db.countries.BR, "2024-03", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 6
+});
+
+db.insert(db.countries.BR, "2024-04", db.dsTypes.AllCarsTotal, "",
+{ "other": 1080
+});
+
+db.insert(db.countries.BR, "2024-04", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 8
+});
+
+db.insert(db.countries.BR, "2024-05", db.dsTypes.AllCarsTotal, "",
+{ "other": 1100
+});
+
+db.insert(db.countries.BR, "2024-05", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 10
+});
+
+db.insert(db.countries.BR, "2024-06", db.dsTypes.AllCarsTotal, "",
+{ "other": 1120
+});
+
+db.insert(db.countries.BR, "2024-06", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 12
+});
+
+db.insert(db.countries.BR, "2024-07", db.dsTypes.AllCarsTotal, "",
+{ "other": 1140
+});
+
+db.insert(db.countries.BR, "2024-07", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 14
+});
+
+db.insert(db.countries.BR, "2024-08", db.dsTypes.AllCarsTotal, "",
+{ "other": 1160
+});
+
+db.insert(db.countries.BR, "2024-08", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 16
+});
+
+db.insert(db.countries.BR, "2024-09", db.dsTypes.AllCarsTotal, "",
+{ "other": 1180
+});
+
+db.insert(db.countries.BR, "2024-09", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 18
+});
+
+db.insert(db.countries.BR, "2024-10", db.dsTypes.AllCarsTotal, "",
+{ "other": 1200
+});
+
+db.insert(db.countries.BR, "2024-10", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 20
+});
+
+db.insert(db.countries.BR, "2024-11", db.dsTypes.AllCarsTotal, "",
+{ "other": 1220
+});
+
+db.insert(db.countries.BR, "2024-11", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 22
+});
+
+db.insert(db.countries.BR, "2024-12", db.dsTypes.AllCarsTotal, "",
+{ "other": 1240
+});
+
+db.insert(db.countries.BR, "2024-12", db.dsTypes.ElectricCarsTotal, "",
+{ "other": 24
+});
---
+++ unittest-tests.js
@@ -0,0 +1,137 @@
+"use strict";
+
+const logOutput = document.createElement("DIV");
+logOutput.style.display = "inline-block";
+logOutput.style.textAlign = "left";
+logOutput.style.marginTop = "5px";
+document.body.appendChild(logOutput);
+
+var allTests = [];
+var currentTest = "";
+var currentTestFailed = false;
+var failedTests = 0;
+
+function log(line, textColor = "") {
+  const div = document.createElement("DIV");
+  if (line == 0)
+    div.innerHTML = "&nbsp;";
+  else
+    div.appendChild(document.createTextNode(line));
+  div.style.color = textColor;
+  logOutput.appendChild(div);
+}
+
+function showSummary() {
+  if (failedTests > 0)
+    log("");
+  log(allTests.length + " tests executed.");
+  if (allTests.length > 0 && failedTests == 0)
+    log("All tests passed.", "green");
+  else if (failedTests == 1)
+    log("1 test failed.", "red");
+  else if (failedTests > 1)
+    log(failedTests + " tests failed.", "red");
+}
+
+function logError(msg) {
+  if (!currentTestFailed) {
+    if (failedTests > 0)
+      log("");
+    log("Test " + currentTest + ":");
+    currentTestFailed = true;
+    failedTests++;
+  }
+  log(msg, "red");
+  console.error(msg);
+}
+
+function assert(value, expectedValue = true) {
+  if (value !== expectedValue)
+    logError("Assertion failed: Expected " + JSON.stringify(expectedValue) + " but got " + JSON.stringify(value) + ".");
+}
+
+function runTest(name, func) {
+  currentTest = name;
+  currentTestFailed = false;
+  if (allTests.includes(name)) {
+    logError("Test name is not unique.");
+    return;
+  }
+  allTests.push(name);
+  func();
+}
+
+function queryChartDataByIncompleteChartConfig(chartConfig) {
+  return db.queryChartData(db.decodeChartConfigString(db.encodeChartConfig(chartConfig)));
+}
+
+try {
+
+db.finalizeDataLoading();
+
+
+// Test cases
+
+runTest("finalizeDataLoading", function() {
+  assert(db.countriesWithData.length, 1);
+  assert(Object.keys(db.countries).length > 10);
+  assert(Object.keys(db.countriesCodes).length, Object.keys(db.countries).length);
+  assert(Object.keys(db.countryNames).length, Object.keys(db.countries).length);
+});
+
+runTest("encodeChartConfig_1", function() {
+  let chartConfig = {};
+  chartConfig.country = "BR";
+  chartConfig.metric = db.metrics.salesElectric;
+  chartConfig.xProperty = db.xProperties.month;
+  chartConfig.timeSpan = "y2020";
+  assert(db.encodeChartConfig(chartConfig), "BR:electric-sales:month:y2020");
+});
+
+runTest("decodeChartConfigString_1", function() {
+  const chartConfig = db.decodeChartConfigString("BR:electric-sales:month:y2020");
+  assert(chartConfig.country, "BR");
+  assert(chartConfig.metric, db.metrics.salesElectric);
+  assert(chartConfig.xProperty, db.xProperties.month);
+  assert(chartConfig.timeSpan, "y2020");
+});
+
+runTest("unfoldChartConfig_1", function() {
+  let chartSetConfig = {};
+  chartSetConfig.country = "DE";
+  chartSetConfig.metric = db.metrics.all;
+  const chartConfigs = db.unfoldChartConfig(chartSetConfig);
+  assert(chartConfigs.length, 6);
+});
+
+runTest("queryChartData_month_1", function() {
+  let chartConfig = {};
+  chartConfig.country = "BR";
+  chartConfig.metric = db.metrics.salesElectric;
+  chartConfig.xProperty = db.xProperties.month;
+  chartConfig.timeSpan = "1y";
+  const chartData = queryChartDataByIncompleteChartConfig(chartConfig);
+  assert(chartData.categories.length, 12);
+  assert(chartData.series.length, 1);
+  assert(chartData.series[0].data.length, 12);
+});
+
+runTest("queryChartData_monthAvg12_2", function() {
+  let chartConfig = {};
+  chartConfig.country = "BR";
+  chartConfig.metric = db.metrics.salesElectric;
+  chartConfig.xProperty = db.xProperties.monthAvg12;
+  chartConfig.timeSpan = "1y";
+  const chartData = queryChartDataByIncompleteChartConfig(chartConfig);
+  assert(chartData.categories.length, 12);
+  assert(chartData.series.length, 1);
+  assert(chartData.series[0].data.length, 12);
+});
+
+
+} catch (err) {
+  logError("Exception raised, see console.");
+  throw err;
+}
+
+showSummary();
