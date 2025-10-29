2429ae3b03494584d4ff116f92b229a1de3bf639
simonkrauter <trustable@disroot.org>
2025-10-29 09:59:04 -0300

    PL: Add data for 2025-09

--- data/data-PL.js
+++ data/data-PL.js
@@ -353,3 +353,11 @@ db.insert(db.countries.PL, "2025-08", db.dsTypes.AllCarsTotal, "https://www.acea
 db.insert(db.countries.PL, "2025-08", db.dsTypes.ElectricCarsTotal, "https://www.acea.auto/files/Press_release_car_registrations_August_2025.pdf",
 { "other": 3306
 });
+
+db.insert(db.countries.PL, "2025-09", db.dsTypes.AllCarsTotal, "https://www.acea.auto/files/Press_release_car_registrations_September_2025.pdf",
+{ "other": 49853
+});
+
+db.insert(db.countries.PL, "2025-09", db.dsTypes.ElectricCarsTotal, "https://www.acea.auto/files/Press_release_car_registrations_September_2025.pdf",
+{ "other": 4452
+});
