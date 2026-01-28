7900ab6bd929045625c6eb116a51c03280fb629a
simonkrauter <trustable@disroot.org>
2026-01-28 15:37:12 -0300

    EU: Add data for 2025-12

--- data/data-EU.js
+++ data/data-EU.js
@@ -853,3 +853,11 @@ db.insert(db.countries.EU, "2025-11", db.dsTypes.AllCarsTotal, "https://www.acea
 db.insert(db.countries.EU, "2025-11", db.dsTypes.ElectricCarsTotal, "https://www.acea.auto/files/Press_release_car_registrations_November_2025.pdf",
 { "other": 188730
 });
+
+db.insert(db.countries.EU, "2025-12", db.dsTypes.AllCarsTotal, "https://www.acea.auto/files/Press_release_car_registrations_December_2025.pdf",
+{ "other": 963319
+});
+
+db.insert(db.countries.EU, "2025-12", db.dsTypes.ElectricCarsTotal, "https://www.acea.auto/files/Press_release_car_registrations_December_2025.pdf",
+{ "other": 217898
+});
