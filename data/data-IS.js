c692638be59dd7a5551068409e7ed6e5a91c937a
simonkrauter <trustable@disroot.org>
2026-07-23 13:55:24 -0300

    IS: Add data for 2026-06

--- data/data-IS.js
+++ data/data-IS.js
@@ -5770,3 +5770,11 @@ db.insert(db.countries.IS, "2026-05", db.dsTypes.AllCarsTotal, "https://www.acea
 db.insert(db.countries.IS, "2026-05", db.dsTypes.ElectricCarsTotal, "https://www.acea.auto/files/Press_release_car_registrations_May_2026.pdf",
 { "other": 789
 });
+
+db.insert(db.countries.IS, "2026-06", db.dsTypes.AllCarsTotal, "https://www.acea.auto/files/Press_release_car_registrations_June_2026.pdf",
+{ "other": 1439
+});
+
+db.insert(db.countries.IS, "2026-06", db.dsTypes.ElectricCarsTotal, "https://www.acea.auto/files/Press_release_car_registrations_June_2026.pdf",
+{ "other": 809
+});
