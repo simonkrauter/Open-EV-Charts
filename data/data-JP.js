1cc9c9e74e7e8a6ee70216a21a7dc9dd4fde852d
simonkrauter <trustable@disroot.org>
2026-09-23 20:41:37 -0300

    Add data for Japan

---
+++ data/data-JP.js
@@ -0,0 +1,451 @@
+// Data sets for car sales in Japan
+
+"use strict";
+
+db.insert(db.countries.JP, "2022-01", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 181926
+});
+
+db.insert(db.countries.JP, "2022-01", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 1691
+});
+
+db.insert(db.countries.JP, "2022-02", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 184729
+});
+
+db.insert(db.countries.JP, "2022-02", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 2281
+});
+
+db.insert(db.countries.JP, "2022-03", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 284544
+});
+
+db.insert(db.countries.JP, "2022-03", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 4219
+});
+
+db.insert(db.countries.JP, "2022-04", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 153509
+});
+
+db.insert(db.countries.JP, "2022-04", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 1610
+});
+
+db.insert(db.countries.JP, "2022-05", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 136405
+});
+
+db.insert(db.countries.JP, "2022-05", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 1572
+});
+
+db.insert(db.countries.JP, "2022-06", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 169680
+});
+
+db.insert(db.countries.JP, "2022-06", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 3379
+});
+
+db.insert(db.countries.JP, "2022-07", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 186711
+});
+
+db.insert(db.countries.JP, "2022-07", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 2169
+});
+
+db.insert(db.countries.JP, "2022-08", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 154316
+});
+
+db.insert(db.countries.JP, "2022-08", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 2034
+});
+
+db.insert(db.countries.JP, "2022-09", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 211585
+});
+
+db.insert(db.countries.JP, "2022-09", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 3279
+});
+
+db.insert(db.countries.JP, "2022-10", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 186202
+});
+
+db.insert(db.countries.JP, "2022-10", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 1953
+});
+
+db.insert(db.countries.JP, "2022-11", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 192904
+});
+
+db.insert(db.countries.JP, "2022-11", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 3268
+});
+
+db.insert(db.countries.JP, "2022-12", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 180792
+});
+
+db.insert(db.countries.JP, "2022-12", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3170/202403251351394699.pdf",
+{ "other": 4137
+});
+
+db.insert(db.countries.JP, "2023-01", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 202255
+});
+
+db.insert(db.countries.JP, "2023-01", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 3415
+});
+
+db.insert(db.countries.JP, "2023-02", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 236704
+});
+
+db.insert(db.countries.JP, "2023-02", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 3594
+});
+
+db.insert(db.countries.JP, "2023-03", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 330162
+});
+
+db.insert(db.countries.JP, "2023-03", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 5149
+});
+
+db.insert(db.countries.JP, "2023-04", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 193042
+});
+
+db.insert(db.countries.JP, "2023-04", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 3309
+});
+
+db.insert(db.countries.JP, "2023-05", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 179804
+});
+
+db.insert(db.countries.JP, "2023-05", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 3280
+});
+
+db.insert(db.countries.JP, "2023-06", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 226760
+});
+
+db.insert(db.countries.JP, "2023-06", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 4110
+});
+
+db.insert(db.countries.JP, "2023-07", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 221656
+});
+
+db.insert(db.countries.JP, "2023-07", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 2679
+});
+
+db.insert(db.countries.JP, "2023-08", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 183299
+});
+
+db.insert(db.countries.JP, "2023-08", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 3121
+});
+
+db.insert(db.countries.JP, "2023-09", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 237816
+});
+
+db.insert(db.countries.JP, "2023-09", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 4818
+});
+
+db.insert(db.countries.JP, "2023-10", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 214683
+});
+
+db.insert(db.countries.JP, "2023-10", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 3382
+});
+
+db.insert(db.countries.JP, "2023-11", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 224586
+});
+
+db.insert(db.countries.JP, "2023-11", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 3331
+});
+
+db.insert(db.countries.JP, "2023-12", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 200630
+});
+
+db.insert(db.countries.JP, "2023-12", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/3169/202403251351136755.pdf",
+{ "other": 3803
+});
+
+db.insert(db.countries.JP, "2024-01", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 193162
+});
+
+db.insert(db.countries.JP, "2024-01", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 2033
+});
+
+db.insert(db.countries.JP, "2024-02", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 202640
+});
+
+db.insert(db.countries.JP, "2024-02", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 2540
+});
+
+db.insert(db.countries.JP, "2024-03", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 268843
+});
+
+db.insert(db.countries.JP, "2024-03", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 3921
+});
+
+db.insert(db.countries.JP, "2024-04", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 181633
+});
+
+db.insert(db.countries.JP, "2024-04", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 1862
+});
+
+db.insert(db.countries.JP, "2024-05", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 176693
+});
+
+db.insert(db.countries.JP, "2024-05", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 2084
+});
+
+db.insert(db.countries.JP, "2024-06", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 211131
+});
+
+db.insert(db.countries.JP, "2024-06", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 3302
+});
+
+db.insert(db.countries.JP, "2024-07", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 229683
+});
+
+db.insert(db.countries.JP, "2024-07", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 2710
+});
+
+db.insert(db.countries.JP, "2024-08", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 180385
+});
+
+db.insert(db.countries.JP, "2024-08", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 2682
+});
+
+db.insert(db.countries.JP, "2024-09", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 240399
+});
+
+db.insert(db.countries.JP, "2024-09", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 3711
+});
+
+db.insert(db.countries.JP, "2024-10", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 231474
+});
+
+db.insert(db.countries.JP, "2024-10", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 2685
+});
+
+db.insert(db.countries.JP, "2024-11", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 222424
+});
+
+db.insert(db.countries.JP, "2024-11", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 3073
+});
+
+db.insert(db.countries.JP, "2024-12", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 184638
+});
+
+db.insert(db.countries.JP, "2024-12", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/5209/202502041555486194.pdf",
+{ "other": 3454
+});
+
+db.insert(db.countries.JP, "2025-01", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 216664
+});
+
+db.insert(db.countries.JP, "2025-01", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 2014
+});
+
+db.insert(db.countries.JP, "2025-02", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 234257
+});
+
+db.insert(db.countries.JP, "2025-02", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 2380
+});
+
+db.insert(db.countries.JP, "2025-03", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 288234
+});
+
+db.insert(db.countries.JP, "2025-03", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 4010
+});
+
+db.insert(db.countries.JP, "2025-04", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 191066
+});
+
+db.insert(db.countries.JP, "2025-04", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 2111
+});
+
+db.insert(db.countries.JP, "2025-05", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 177980
+});
+
+db.insert(db.countries.JP, "2025-05", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 2848
+});
+
+db.insert(db.countries.JP, "2025-06", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 217333
+});
+
+db.insert(db.countries.JP, "2025-06", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 4264
+});
+
+db.insert(db.countries.JP, "2025-07", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 220544
+});
+
+db.insert(db.countries.JP, "2025-07", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 2908
+});
+
+db.insert(db.countries.JP, "2025-08", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 159854
+});
+
+db.insert(db.countries.JP, "2025-08", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 2649
+});
+
+db.insert(db.countries.JP, "2025-09", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 229510
+});
+
+db.insert(db.countries.JP, "2025-09", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 4484
+});
+
+db.insert(db.countries.JP, "2025-10", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 214454
+});
+
+db.insert(db.countries.JP, "2025-10", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 3908
+});
+
+db.insert(db.countries.JP, "2025-11", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 201999
+});
+
+db.insert(db.countries.JP, "2025-11", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 4209
+});
+
+db.insert(db.countries.JP, "2025-12", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 181628
+});
+
+db.insert(db.countries.JP, "2025-12", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/6965/202604021716334815.pdf",
+{ "other": 4100
+});
+
+db.insert(db.countries.JP, "2026-01", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 200047
+});
+
+db.insert(db.countries.JP, "2026-01", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 5419
+});
+
+db.insert(db.countries.JP, "2026-02", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 211362
+});
+
+db.insert(db.countries.JP, "2026-02", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 6245
+});
+
+db.insert(db.countries.JP, "2026-03", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 265438
+});
+
+db.insert(db.countries.JP, "2026-03", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 11245
+});
+
+db.insert(db.countries.JP, "2026-04", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 223369
+});
+
+db.insert(db.countries.JP, "2026-04", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 6937
+});
+
+db.insert(db.countries.JP, "2026-05", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 187486
+});
+
+db.insert(db.countries.JP, "2026-05", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 8957
+});
+
+db.insert(db.countries.JP, "2026-06", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 247956
+});
+
+db.insert(db.countries.JP, "2026-06", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 12225
+});
+
+db.insert(db.countries.JP, "2026-07", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 242578
+});
+
+db.insert(db.countries.JP, "2026-07", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 10062
+});
+
+db.insert(db.countries.JP, "2026-08", db.dsTypes.AllCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 172839
+});
+
+db.insert(db.countries.JP, "2026-08", db.dsTypes.ElectricCarsTotal, "https://www.jada.or.jp/files/libs/7572/202609030957113390.pdf",
+{ "other": 8351
+});
